5721e9c8dcbe92fdb16775f6a88ef51cb31b7cbe
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-01-13 22:28:14 +0100

    JS: line 13-14 (print into HTML input and image using Vue.js data)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 
+//BONUS
+// 1. Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+// 2. Inserire un input, l’utente scrive il suo nome, 
+    //salutate l’utente all’interno di uno span. Ciao + valore input.
+// 3. Inventate e divertitevi
+
 new Vue ({
     el: "#app",
-    
     data: {
-        message: 'Hello Vue.js! Questo è un messaggio stampato utilizzando i data di Vue.js.',
-    }
+        message: 'Ciao! Questo è un messaggio stampato utilizzando i data di Vue.js.',
+        image: 'https://images.unsplash.com/photo-1607706189992-eae578626c86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+        name: '',
+    },
+    
 });
