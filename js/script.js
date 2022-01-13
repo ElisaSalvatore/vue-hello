f7c638ce647b5c76984d5efb8fbcda4f7afe9bc1
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-01-13 15:12:21 +0100

    print message into HTML using Vue.js data

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+new Vue ({
+    el: "#app",
+    
+    data: {
+        message: 'Hello Vue.js! Questo è un messaggio stampato utilizzando i data di Vue.js.',
+    }
+});
